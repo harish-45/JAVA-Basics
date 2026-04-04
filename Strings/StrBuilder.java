a54d16e26e97627277a899906fc4e7808290d774
harry <stackharry45@gmail.com>
2026-04-04 20:49:11 +0530

    StringBuffer in java basic understanding

--- Strings/StrBuilder.java
+++ Strings/StrBuilder.java
@@ -1,6 +1,8 @@
 public class StrBuilder {
     public static void main(String[] args) {
-        StringBuilder series = new StringBuilder(" Hello ");
+
+        // use capacity when you know the length of String else you can use 'string' as well
+        StringBuilder series = new StringBuilder(30);
         for (int i = 0; i < 26; i++) {
             char ch = (char) ('a' + i);
 
